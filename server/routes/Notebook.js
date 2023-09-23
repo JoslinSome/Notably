aac0e076c3e5fd4eea5e65b32a91fec7c5bb0101
Ben DeSollar <ben.desollar01@gmail.com>
2023-09-23 12:35:28 -0500

    Updates to index and made code prettier

--- server/routes/Notebook.js
+++ server/routes/Notebook.js
@@ -1,41 +1,48 @@
 import express from "express";
-import {notebookModel} from "../models/Notebooks.js";
+import { notebookModel } from "../models/Notebooks.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/create",async (req,res)=>{
-    const {title,description} = req.body
-    const date = new Date();
-    const notebook = new notebookModel({
-        title,description, created: date, modified: date
-    })
-    await notebook.save()
-    res.send(notebook)
-})
+router.post("/create", async (req, res) => {
+  const { title, description } = req.body;
+  const date = new Date();
+  const notebook = new notebookModel({
+    title,
+    description,
+    created: date,
+    modified: date,
+  });
+  await notebook.save();
+  res.send(notebook);
+});
 
-router.put("/update",async (req,res)=>{
-    const {id,title,description} = req.body
-    const date = new Date();
-    const notebook = await notebookModel.findByIdAndUpdate(id,{title,description,modified: date})
-    await notebook.save()
-    res.send(notebook)
-})
+router.put("/update", async (req, res) => {
+  const { id, title, description } = req.body;
+  const date = new Date();
+  const notebook = await notebookModel.findByIdAndUpdate(id, {
+    title,
+    description,
+    modified: date,
+  });
+  await notebook.save();
+  res.send(notebook);
+});
 
-router.get("/get-all",async (req,res)=>{
-    const notebooks = await notebookModel.find({})
-    res.send(notebooks)
-})
+router.get("/get-all", async (req, res) => {
+  const notebooks = await notebookModel.find({});
+  res.send(notebooks);
+});
 
-router.get("/get-by-id",async (req,res)=>{
-    const {id} = req.body
-    const notebook = await notebookModel.findById(id)
-    res.send(notebook)
-})
+router.get("/get-by-id", async (req, res) => {
+  const { id } = req.body;
+  const notebook = await notebookModel.findById(id);
+  res.send(notebook);
+});
 
-router.delete("/delete",async (req,res)=>{
-    const {id} = req.body
-    const notebook = await notebookModel.findByIdAndDelete(id)
-    res.send(notebook)
-})
+router.delete("/delete", async (req, res) => {
+  const { id } = req.body;
+  const notebook = await notebookModel.findByIdAndDelete(id);
+  res.send(notebook);
+});
 
-export {router as NotebookRouter}
+export { router as NotebookRouter };
