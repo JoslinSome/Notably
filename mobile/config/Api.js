a9cee06ef4f919812ff7dac5ee68ce9fcb8330e6
Ben DeSollar <ben-desollar@uiowa.edu>
2023-09-24 11:07:33 -0500

    Update Api.js

--- mobile/config/Api.js
+++ mobile/config/Api.js
@@ -1,7 +1,8 @@
 import Constants from "expo-constants";
 
 const { expoConfig } = Constants;
-
-const api ="172.17.64.197:3002"
-console.log(api)
+const api =
+  typeof expoConfig.packagerOpts === `object` && expoConfig.packagerOpts.dev
+    ? expoConfig.debuggerHost.split(`:`).shift().concat(`:3002`)
+    : `api.example.com`;
 export { api };
